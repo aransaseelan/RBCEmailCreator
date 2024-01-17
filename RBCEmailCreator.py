80bd68aa4430e5ed78124a9a1e4bb170740c6392
Aran Saseelan <aran.saseelan@gmail.com>
2024-01-16 21:53:20 -0500

    Made it so it can read text files

--- RBCEmailCreator.py
+++ RBCEmailCreator.py
@@ -1,16 +1,22 @@
-numberofPeople = int(input("How many people's emails do you need."))
+with open('RBC CUSEC Name List.txt', 'r') as file:
+    data = file.readlines() #readlines reads the file line by line and returns a list of strings
+    print(data)
+    firstnames = []
+    lastnames = []
+    for line in data: 
+        parts = line.strip().split() #strip takes out the white spaces in the front and back, split takes out the white spaces in the middle
+        if len(parts) >= 2:
+            firstnames.append(parts[0])
+            lastnames.append(parts[1])
 
-firstnames = []
-lastnames = []
+with open('RBCEmail.txt', 'w') as output_file:
+    for i in range(len(firstnames)):
+        output_file.write(firstnames[i] + "." + lastnames[i] + "@rbc.com" + "\n")
 
+# Move the print statement outside the loop
+with open('RBCEmail.txt', 'r') as output_file:
+    output = output_file.read()
+    print(output)
 
-for i in range(numberofPeople):
-    firstnames.append(input("Please enter the firstname. \n"))
-    lastnames.append(input("Please enter the lastname. \n"))
-
-
-print("Here are your emails!!")
-for i in range(numberofPeople):
-    print(firstnames[i] + "." + lastnames[i] + "@rbc.com" + "\n")
 
 #Email is formated first.last@rbc.com
